a6bb62ed603f4dd88972bd80a3e0acef47e529f7
Salahuddin-dat <salahuddinshaar@gmail.com>
2021-09-15 05:56:12 +0100

    edit req

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from flask import Flask, flash, request, redirect, url_for, render_template
-import urllib.request
 import os
 from werkzeug.utils import secure_filename
-
+from analyze import FaceAnalyze
 app = Flask(__name__)
 
 UPLOAD_FOLDER = 'static/uploads/'
@@ -13,6 +12,7 @@ app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
+analyze = FaceAnalyze()
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -34,6 +34,7 @@ def upload_image():
         return redirect(request.url)
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
+
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         # print('upload_image filename: ' + filename)
         flash('Image successfully uploaded and displayed below')
